c3b47924d6a9f4c10c71326bdbe75c3084c9d68f
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2023-12-10 15:50:40 +0700

    add classification form

--- main.js
+++ main.js
@@ -1,18 +1,49 @@
+/*
+Preview Images For Segmentation & Classification
+*/
 const imgToBeSegmented = document.getElementById('imgToBeSegmented');
 const imgToBeSegmentedPreview = document.getElementById('imgToBeSegmentedPreview');
+const imgToBeClassified = document.getElementById('imgToBeClassified');
+const imgToBeClassifiedPreview = document.getElementById('imgToBeClassifiedPreview');
 
-imgToBeSegmented.addEventListener('change', function () {
-  const file = this.files[0];
+function setImgPreview(file, elemen) {
   if(file){
     const reader = new FileReader();
     reader.addEventListener('load', function () {
-      imgToBeSegmentedPreview.src = reader.result;
-      imgToBeSegmentedPreview.style.display = 'block';
+      elemen.src = reader.result;
+      elemen.style.display = 'block';
     });
     reader.readAsDataURL(file);
   }
+}
+
+imgToBeSegmented.addEventListener('change', function () {
+  const file = this.files[0];
+  setImgPreview(file, imgToBeSegmentedPreview);
 });
 
+imgToBeClassified.addEventListener('change', function () {
+  const file = this.files[0];
+  setImgPreview(file, imgToBeClassifiedPreview);
+});
+
+/*
+Tiny itsy bitsy teeny weeny checkbox classification based on segmentation
+*/
+const useSegmentationCheck = document.getElementById('useSegmentationCheck');
+useSegmentationCheck.addEventListener('change', function () {
+  // if checked then disable the classification input
+  classificationInputField = document.getElementById('classificationInputField');
+  if(this.checked){
+    classificationInputField.disabled = true;
+  } else {
+    classificationInputField.disabled = false;
+  }
+});
+
+/*
+Form Handler For Segmentation & Classification
+*/
 const segmentationForm = document.getElementById('segmentationForm');
 segmentationForm.addEventListener('submit', function (event) {
   event.preventDefault(); // Prevent the default form submission
@@ -21,6 +52,9 @@ segmentationForm.addEventListener('submit', function (event) {
   segmentationApiRequest();
 });
 
+/*
+Classification & Segmentation API Request
+*/
 function classificationRequest() {
   const apiUrl = 'https://ravindrawiguna-classification-coagulation.hf.space/upload';
   fetch(apiUrl, {
